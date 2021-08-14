485ce66cbf67138e7d3aee2763f27693e2ceb7ec
wzheian21219014 <52736736+wzheian21219014@users.noreply.github.com>
2021-08-14 14:32:26 +0800

    Add files via upload

---
+++ src/PreInfo(ACG).h
@@ -0,0 +1,49 @@
+void SetColor(short x)
+{
+  if(x>=0 && x<=15)
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), x);
+  else
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+}
+
+void SetColor(short x,short y)
+{
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), x+16*y);
+}
+
+void SetSize(short x,short y){
+	HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	SMALL_RECT rc = {1,1,x,y};
+	SetConsoleWindowInfo(hOut ,true ,&rc);
+}
+
+
+void Shake(int power,int time){
+	int shake_time = 25;
+    int shake_distance = power;
+    RECT rect;
+    HWND window = NULL, oldwindow = NULL;
+    int x, y, width, height;
+    int i;
+    for (i = 0; i < time; i++) {
+        window = GetForegroundWindow();
+        if (window != oldwindow) {
+            GetWindowRect(window, &rect);
+            x = rect.left;
+            y = rect.top;
+            width = rect.right - x;
+            height = rect.bottom - y;
+            oldwindow = window;
+        }
+        MoveWindow(window, x - shake_distance, y, width, height, TRUE); 
+        Sleep(shake_time); 
+        MoveWindow(window, x - shake_distance, y - shake_distance, width, height, TRUE);
+        Sleep(shake_time);
+        MoveWindow(window, x, y - shake_distance, width, height, TRUE);
+        Sleep(shake_time);
+        MoveWindow(window, x, y, width, height, TRUE);
+        Sleep(shake_time);
+    }
+    return;
+}
+
